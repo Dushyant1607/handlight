2f87fad4317fd72bd0b6c029608aca565e947edb
Dushyant1607 <dushyantdhote708@gmail.com>
2026-04-11 22:37:59 +0530

    feat: integrate mediapipe Hands for landmark detection

---
+++ hand_tracker.py
@@ -0,0 +1,14 @@
+import cv2
+import mediapipe as mp
+from config import MIN_DETECTION_CONFIDENCE, MIN_TRACKING_CONFIDENCE, MAX_HANDS
+
+
+class HandTracker:
+    def __init__(self):
+        self.mp_hands = mp.solutions.hands
+        self.hands = self.mp_hands.Hands(
+            static_image_mode=False,
+            max_num_hands=MAX_HANDS,
+            min_detection_confidence=MIN_DETECTION_CONFIDENCE,
+            min_tracking_confidence=MIN_TRACKING_CONFIDENCE,
+        )
