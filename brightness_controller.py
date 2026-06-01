3d4aaf259afe92e46de4b308bc8f953ba938f51e
Dushyant1607 <dushyantdhote708@gmail.com>
2026-06-01 13:24:51 +0530

    feat: add exponential smoothing filter

--- brightness_controller.py
+++ brightness_controller.py
@@ -1,4 +1,5 @@
 import screen_brightness_control as sbc
+from config import SMOOTHING_ALPHA, BRIGHTNESS_DELTA_THRESHOLD  
 
 
 class BrightnessController:
@@ -9,4 +10,9 @@ class BrightnessController:
     def set_target(self, value: float):
         clamped = max(0, min(100, int(value)))
         self._target = clamped
-        sbc.set_brightness(clamped, display=0)            
+
+        smoothed = SMOOTHING_ALPHA * clamped + (1 - SMOOTHING_ALPHA) * self._current
+
+        if abs(smoothed - self._current) > BRIGHTNESS_DELTA_THRESHOLD:
+            sbc.set_brightness(int(smoothed), display=0)
+            self._current = smoothed
